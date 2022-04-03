f78681e7cf7d63d09192bf00307df04d80bb08b0
Donda <34602885+dondasasmita@users.noreply.github.com>
2022-04-03 14:34:06 +0800

    Add inputs to populate the div attributes

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,42 +2,114 @@ import { useState } from 'react';
 import Result from './Result';
 
 const Form = () => {
-	const [name, setName] = useState('');
-	const [data, setData] = useState('');
+	const [cssClassName, setCssClassName] = useState('');
+	const [markUpId, setMarkupId] = useState('');
+	const [device, setDevice] = useState('');
+	const [unit, setUnit] = useState('');
 	const [result, setResult] = useState('');
 
 	const handleSubmit = e => {
 		e.preventDefault();
-		setResult({ name, data });
+		setResult({ cssClassName, markUpId, device, unit });
 		resetForm();
 	};
 
 	const resetForm = () => {
-		setName('');
-		setData('');
+		setCssClassName('');
+		setMarkupId('');
+		setDevice('');
+		setUnit('');
 	};
+
+	/**
+	 * Below could be anything
+	 */
+	const DEVICES = ['desktop', 'mobile', 'tablet'];
+	const UNITS = ['demo-1', 'demo-2', 'demo-3', 'demo-4'];
 
 	return (
 		<div className="columns">
 			<div className="column">
 				<form onSubmit={handleSubmit}>
-					<label>
-						<input
-							placeholder="classname"
-							id="classInput"
-							type="text"
-							value={name}
-							onChange={e => setName(e.target.value)}
-						/>
-						<input
-							placeholder="jsOptions"
-							id="dataInput"
-							type="text"
-							value={data}
-							onChange={e => setData(e.target.value)}
-						/>
-					</label>
-					<input className="button is-primary" type="submit" value="Submit" />
+					<div className="columns">
+						<div className="column">
+							<div className="field">
+								<label className="label">Classname</label>
+								<div className="control">
+									<input
+										className="input"
+										type="text"
+										placeholder="Define classname"
+										value={cssClassName}
+										onChange={e => setCssClassName(e.target.value)}
+									></input>
+								</div>
+							</div>
+						</div>
+						<div className="column">
+							<div className="field">
+								<label className="label">ID</label>
+								<div className="control">
+									<input
+										className="input"
+										type="text"
+										value={markUpId}
+										placeholder="Define ID"
+										onChange={e => setMarkupId(e.target.value)}
+									></input>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="columns">
+						<div className="column">
+							<div className="field">
+								<label className="label">Device</label>
+								<div className="control">
+									<div className="select">
+										<select
+											id="deviceInput"
+											value={device}
+											onChange={e => {
+												setDevice(e.target.value);
+											}}
+										>
+											<option />
+											{DEVICES.map(device => (
+												<option key={device} value={device}>
+													{device}
+												</option>
+											))}
+										</select>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="column">
+							<div className="field">
+								<label className="label">Unit</label>
+								<div className="control">
+									<div className="select">
+										<select
+											id="unitInput"
+											value={unit}
+											onChange={e => {
+												setUnit(e.target.value);
+											}}
+										>
+											<option />
+											{UNITS.map(unit => (
+												<option key={unit} value={unit}>
+													{unit.toLocaleUpperCase()}
+												</option>
+											))}
+										</select>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<input className="button is-primary" type="submit" value="Generate" />
 				</form>
 			</div>
 			<Result result={result} />
